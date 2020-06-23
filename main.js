5ae92614d4cdf372cf900fde70d7ed981475cc2d
Tigerly <tigerly@protonmail.com>
2020-06-23 00:52:34 -0700

    Tinkering around with movement

--- main.js
+++ main.js
@@ -1,13 +1,65 @@
 var mineflayer = require('mineflayer');
+var Vec3 = require('vec3').Vec3;
+
 var bot = mineflayer.createBot({
   host: "localhost", // optional
   port: 25565,       // optional
-  username: "Steven't", // email and password are required only for
+  username: "Stevent", // email and password are required only for
   version: false                 // false corresponds to auto version detection (that's the default), put for example "1.8.8" if you need a specific version
 });
 
 bot.on('chat', function(username, message) {
-  if (username === bot.username) return;
-  bot.chat(message);
+  if(message == "move")
+  {
+    var targetLoc = bot.players[username].entity.position;
+    moveTo(targetLoc);
+  }
 });
 bot.on('error', err => console.log(err))
+
+var Moving = false;
+var MoveToLoc = null;
+var successDistance = 0.3;
+var failDistance = 0;
+
+function moveTo(position) {
+  MoveToLoc = position.clone();
+  Moving = true;
+  var startLoc = bot.entity.position;
+
+  console.log("Moving from "+startLoc+" to " + MoveToLoc);
+  failDistance = MoveToLoc.distanceTo(startLoc) + 2;
+  
+  bot.lookAt(MoveToLoc, true, function () { 
+    console.log("LookAt finished");
+    bot.setControlState("forward",true); 
+  });
+
+}
+
+bot.on("move", function () {
+  if(Moving)
+  {
+    var dist = bot.entity.position.distanceTo(MoveToLoc);
+    console.log("distance = " + dist);
+    if(dist < successDistance)
+    {
+      Moving = false;
+      bot.clearControlStates();
+      console.log("Completed movement.");
+      bot.chat("Completed movement.");
+    }
+    else if(dist > failDistance)
+    {
+      Moving = false;
+      bot.clearControlStates();
+      console.log("Failed in movement!");
+      bot.chat("Something went terribly wrong!");
+    }
+    else
+    {
+      bot.lookAt(MoveToLoc, true, function () { } );
+    }
+  }
+
+});
